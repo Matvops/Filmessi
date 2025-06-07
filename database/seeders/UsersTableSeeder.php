fcc5f92fa891342cc8bae75263fd7695ee36f6f5
Matheus <matheusmartinscia@gmail.com>
2025-06-06 22:22:51 -0300

    Feat: Cria seeder de users

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        for($i = 0; $i < 3; $i++) {
+            DB::table('users')->insert(
+                [
+                    'username' => "user$i",
+                    'email' => "user$i@gmail.com",
+                    'password' => bcrypt("Senha123"),
+                    'role' =>  "ADMIN",
+                    'accept_term' => true,
+                    'email_verified_at' => Carbon::now(),
+                    'active' => true,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ]
+            );
+        }
+    }
+}
