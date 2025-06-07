2426fb44f6836030e25afe28bccd5bb675435a97
Matheus <matheusmartinscia@gmail.com>
2025-06-07 19:29:31 -0300

    Feat: Cria rota para logout

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,5 @@ Route::middleware('auth')->group(function(){
     Route::get('/home', function() {
         echo json_encode(Auth::user());
     })->name('home');
+    Route::get('/logout', [AuthController::class, 'logout'])->name("logout");
 });
