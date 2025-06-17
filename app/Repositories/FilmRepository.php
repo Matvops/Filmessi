5b942e16884c171b100ee80ba566b567cb5bb8ef
Matheus <matheusmartinscia@gmail.com>
2025-06-17 20:43:29 -0300

    Feat: Cria método para retornar os 8 últimos filmes adicionados

--- app/Repositories/FilmRepository.php
+++ app/Repositories/FilmRepository.php
@@ -2,6 +2,21 @@
 
 namespace App\Repositories;
 
+use App\Models\Film;
+use Illuminate\Database\Eloquent\Collection;
+
 class FilmRepository {
-    
+
+    public function getNewMovies(): Collection|null
+    {
+        return Film::with('category')
+                        ->where('active', true)
+                        ->whereNull('deleted_at')
+                        ->orderBy('year', 'DESC')
+                        ->orderBy('created_at', 'DESC')
+                        ->limit(8)
+                        ->get();
+
+        
+    }
 }
