334c67e23a4254c046f62dbe556378822a5a3482
Matheus <matheusmartinscia@gmail.com>
2025-06-17 20:43:59 -0300

    Feat: Adiciona método para retornar filmes

--- app/Services/MainService.php
+++ app/Services/MainService.php
@@ -2,7 +2,18 @@
 
 namespace App\Services;
 
+use App\Repositories\FilmRepository;
+
 class MainService {
     
-    
+    private FilmRepository $filmRepository;
+
+    function __construct()
+    {
+       $this->filmRepository = new FilmRepository; 
+    }
+
+    public function getNewMovies(){
+        return $this->filmRepository->getNewMovies();
+    }
 }
