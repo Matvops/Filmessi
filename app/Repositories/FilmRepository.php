dcd3375124b6e25098d453ed3327138b101e4c06
Matheus <matheusmartinscia@gmail.com>
2025-06-20 19:37:54 -0300

    Feat: Cria método getMostVisitMovies

--- app/Repositories/FilmRepository.php
+++ app/Repositories/FilmRepository.php
@@ -7,16 +7,25 @@ use Illuminate\Database\Eloquent\Collection;
 
 class FilmRepository {
 
-    public function getNewMovies(): Collection|null
+    public function getNewMovies(): Collection
     {
-        return Film::with('category')
-                        ->where('active', true)
+        return Film::where('active', true)
                         ->whereNull('deleted_at')
                         ->orderBy('year', 'DESC')
                         ->orderBy('created_at', 'DESC')
                         ->limit(8)
                         ->get();
-
         
     }
+
+    public function getMostVisitMovies(): Collection
+    {
+        return Film::where('active', true)
+                        ->whereNull('deleted_at')
+                        ->orderBy('views', 'DESC')
+                        ->limit(8)
+                        ->get();
+
+    }
+
 }
