c0b4f4ff80931b9621d87fa92b98bf6750e6088e
Matheus <matheusmartinscia@gmail.com>
2025-06-27 22:55:50 -0300

    Feat: Cria icone para favoritar/desfavoritar filme

--- resources/views/show_film.blade.php
+++ resources/views/show_film.blade.php
@@ -1,10 +1,18 @@
 <x-layouts.main_layout title="Filme genérico">
     <x-slot:content>
         <header class="bg-top-header">
+            
             <div class="d-flex justify-content-between align-items-center py-2 px-3">
                 <h1>
                     <a href="{{ route('home') }}" class="text-light display-2 text-decoration-none">FILMESSI</a>
-                <h1>
+                </h1>
+
+                @if(session('error_favorite'))
+                    <div class="apaga-toast p-3 rounded-1 bg-black text-danger text-center container w-25 fs-4">
+                        {{session('error_favorite')}}
+                    </div>
+                @endif
+
                 @guest
                     <div>
                         <a href="{{ route('login') }}" class="btn btn-primary px-4 py-2 fs-5">Entrar</a>
@@ -42,27 +50,45 @@
             <div class="bg-dark-blue p-5">
                 <div class="d-flex justify-content-between align-items-center border-bottom px-1 pb-3 mb-4">
                     <h1 class="text-light">{{ $film->title }}</h1>
-                
-                    <p class="text-light">Favorito</p>
-                </div>
-                <div class="d-flex justify-content-between align-items-center gap-5 ">
-                    <div>
-                    <div class="card-content">
-                            <img src="{{asset("$film->image_path")}}" alt="{{$film->title}}" class="card-container_image">
+                    
+                    <form method="post" action="{{route('favorite')}}">
+                        @csrf
+                        <input type="hidden" name="favorite" value="{{$film->favorite}}">
+                        <input type="hidden" name="film_id" value="{{$film->film_id}}">      
+                        
+                        @if($film->favorite)
+                        <button type="submit" class="btn">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="yellow" class="bi bi-bookmark-fill" viewBox="0 0 16 16">
+                                    <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2"/>
+                                </svg>
+                            </button>
+                            @else
+                            <button type="submit" class="btn">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="yellow" class="bi bi-bookmark" viewBox="0 0 16 16">
+                                    <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z"/>
+                                </svg>
+                            </button>
+                            @endif
+                        </form>
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center gap-5 ">
+                        <div>
+                            <div class="card-content">
+                                <img src="{{asset("$film->image_path")}}" alt="{{$film->title}}" class="card-container_image">
+                            </div>
+                        </div>
+                        <div>
+                            <p class="text-light fs-4 fw-medium">{{$film->description}}</p>
+                            
+                            <a href="{{$film->link}}" class="btn btn-info text-light fs-4 fw-semibold text-decoration-none" target="blank">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
+                                    <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"/>
+                                </svg> 
+                                PLAY
+                            </a>
                         </div>
                     </div>
-                    <div>
-                        <p class="text-light fs-4 fw-medium">{{$film->description}}</p>
-
-                        <a href="{{$film->link}}" class="btn btn-info text-light fs-4 fw-semibold text-decoration-none" target="blank">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
-                                <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"/>
-                            </svg> 
-                            PLAY
-                        </a>
-                    </div>
                 </div>
-            </div>
-        </main>
-    </x-slot:content>
-</x-layouts.main_layout>
+            </main>
+        </x-slot:content>
+    </x-layouts.main_layout>
