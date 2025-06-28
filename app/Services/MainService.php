0bc2b5cef2bf72c6256df6311bfa98986c073924
Matheus <matheusmartinscia@gmail.com>
2025-06-27 22:57:18 -0300

    Feat: Cria método para favoritar/desfavoritar filme

--- app/Services/MainService.php
+++ app/Services/MainService.php
@@ -3,17 +3,23 @@
 namespace App\Services;
 
 use App\Exceptions\NotFoundException;
+use App\Models\Favorite;
 use App\Repositories\FilmRepository;
+use App\Repositories\UserRepository;
 use App\Utils\Response;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\DB;
 
 class MainService {
     
     private FilmRepository $filmRepository;
+    private UserRepository $userRepository;
 
     function __construct()
     {
        $this->filmRepository = new FilmRepository; 
+       $this->userRepository = new UserRepository;
     }
 
     public function getMainMovies() {
@@ -40,9 +46,39 @@ class MainService {
             $id = Crypt::decrypt($token);
 
             $film = $this->filmRepository->getFilmById($id);
-
+            $user = $this->userRepository->getUserByEmail(Auth::user()->email);
+            $film->favorite = $this->filmRepository->isFavorite($id,  $user->user_id);
+           
             return Response::getResponse(true, '', $film);
         } catch (NotFoundException $e) {
+            return Response::getResponse(false, $e->getMessage());
+        }
+    }
+
+    public function favoriteFilm($isFavorite, $filmIdEcnrypted): Response
+    {
+        try {
+
+            $filmId = Crypt::decrypt($filmIdEcnrypted);
+            $user = $this->userRepository->getUserByEmail(Auth::user()->email);
+
+            DB::beginTransaction();
+            
+            if($isFavorite) {
+                Favorite::where('favorite_film_id', $filmId)
+                        ->where('favorite_user_id', $user->user_id)
+                        ->delete();
+            } else {
+                $favorite = new Favorite();
+                $favorite->favorite_film_id = $filmId;
+                $favorite->favorite_user_id = $user->user_id;
+                $favorite->save();
+            }
+
+            DB::commit();
+            return Response::getResponse(true, '');
+        } catch (NotFoundException $e) {
+            DB::rollBack();
             return Response::getResponse(false, $e->getMessage());
         }
     }
