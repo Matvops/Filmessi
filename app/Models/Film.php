ae573d11940f450f72a94de746a5568e224a3602
Matheus <matheusmartinscia@gmail.com>
2025-06-27 22:29:30 -0300

    Feat: Cria relação muitos para muitos com Users

--- app/Models/Film.php
+++ app/Models/Film.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Support\Facades\Crypt;
 
@@ -31,9 +32,13 @@ class Film extends Model
     protected function filmId(): Attribute
     {
         return Attribute::make(
-            get: fn ($value) => Crypt::encrypt($value),
-            set: fn ($value) => Crypt::decrypt($value)
+            get: fn ($value) => Crypt::encrypt($value)
         );
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'favorites', 'favorite_film_id', 'favorite_user_id');
     }
 
     public function category(): HasOne
