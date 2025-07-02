2dbef02615db15ad144d3bc29980eec363a24620
Matheus <matheusmartinscia@gmail.com>
2025-07-02 19:51:58 -0300

    Feat: Passa filmes para view do painel

--- app/Http/Controllers/PanelController.php
+++ app/Http/Controllers/PanelController.php
@@ -2,15 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\PanelService;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class PanelController extends Controller
 {
+    private PanelService $panelService;
+
+    function __construct(PanelService $service)
+    {
+        $this->panelService = $service;
+    }
     
     public function panel(): View
     {
-        return view('panel');
+        $films = $this->panelService->getAllFilms();
+
+        return view('panel', ['films' => $films]);
     }
 
     public function register(): View
