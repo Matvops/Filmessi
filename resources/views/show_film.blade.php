0d5565e8ee7665c7e9e2d3fa9a21832a1a107892
Matheus <matheusmartinscia@gmail.com>
2025-07-05 16:53:32 -0300

    Fix: Corrige identação

--- resources/views/show_film.blade.php
+++ resources/views/show_film.blade.php
@@ -18,13 +18,13 @@
                                     <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2"/>
                                 </svg>
                             </button>
-                            @else
+                        @else
                             <button type="submit" class="btn">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="yellow" class="bi bi-bookmark" viewBox="0 0 16 16">
                                     <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z"/>
                                 </svg>
                             </button>
-                            @endif
+                        @endif
                         </form>
                     </div>
                     <div class="d-flex justify-content-between align-items-center gap-5 ">
