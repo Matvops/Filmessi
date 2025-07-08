3436a0f46ca8074de367e8e713565b108c5192dd
Matheus <matheusmartinscia@gmail.com>
2025-07-07 21:37:12 -0300

    Feat: Remove métodos do MainController passados ao FavoriteController

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\FavoriteFilmRequest;
 use App\Services\MainService;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
 
 class MainController extends Controller
@@ -20,25 +18,5 @@ class MainController extends Controller
     {
         $movies = $this->mainService->getMainMovies();
         return view('home', ['movies' => $movies]);
-    }
-
-    public function favoriteFilm(FavoriteFilmRequest $request): RedirectResponse
-    {
-        $response = $this->mainService->favoriteFilm($request->favorite, $request->film_id);
-
-        if(!$response->getStatus()) {
-            return back()
-                    ->withInput()
-                    ->with('error_favorite', $response->getMessage());
-        }
-
-        return back();
-    }
-
-    public function showFavorites(): View
-    {
-        $response = $this->mainService->getFavorites();
-
-        return view('favorites', ['films' => $response->getData()]);
     }
 }
